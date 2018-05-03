2c97e4520f047f4a33cee08612f80c4dee39a12f
cauez0rd <caue_bc@yahoo.com.br>
2018-05-03 15:44:13 -0300

    (very) basic CRUD done. All CRUD functions working (at least on my machine...)

--- angularjs/js/controller/cadastroCtrl.js
+++ angularjs/js/controller/cadastroCtrl.js
@@ -2,51 +2,88 @@ myApp.controller('CadastroCtrl', ['$scope', '$http', function($scope, $http) {
     let apiURL = 'http://localhost:8000/users/';
 
     let getAllUsers = $http.get(apiURL)
-                                        .then(function successCallback(response) {
-                                            $scope.users = response.data;
-                                            console.log("deu certooo");
-                                            console.log(response.status, response.statusText);
-                                            console.log($scope.users);
-                                        }, function errorCallback(response) {
-                                            console.log("Deu ruim :(((");
-                                            console.log(response.status, response.statusText);
-                                        }    
+                                    .then(function successCallback(response) {
+                                        $scope.users = response.data;
+                                        console.log("deu certooo");
+                                        console.log(response.status, response.statusText);
+                                        console.log($scope.users);
+                                    }, function errorCallback(response) {
+                                        console.log("Deu ruim :(((");
+                                        console.log(response.status, response.statusText);
+                                    }
     )
 
-    $scope.editUser = function(user) {
-        let datetimeNow = Date();
-        console.log(datetimeNow);
-        $scope.userBeingEdited = angular.copy(user);
-        $scope.userBeingEdited.viewed_at = '2018-05-01T19:47:25.140929Z'
-        console.log($scope.userBeingEdited)
-        $scope.userURL = `${apiURL}${user.id}/`;
-        //testando post $scope.userURL = `${apiURL}`;
+    $scope.updateAllUsers = function() {
+        $http.get(apiURL)
+            .then(function successCallback(response) {
+                $scope.users = response.data;
+                console.log("deu certooo");
+                console.log(response.status, response.statusText);
+                console.log($scope.users);
+            }, function errorCallback(response) {
+                console.log("Deu ruim :(((");
+                console.log(response.status, response.statusText);
+            }
+            )
+    }
 
-        // atualiza o viewed_at com o momento em que o botão de editar é clicado
-        $http.put($scope.userURL, $scope.userBeingEdited)
-                                .then(function successCallback(response) {
-                                    console.log(response.status, response.statusText);
-                                }, function errorCallback(response) {
-                                    console.log("Deu ruim :(((");
-                                    console.log(response.status, response.statusText);
-                                })
-        
+    $scope.viewUser = function(user) {
+        $scope.userBeingViewed = user;
+        $scope.userURL = `${apiURL}${user.id}/`;
+        $http.put($scope.userURL, $scope.userBeingViewed)
+            .then(function successCallback(response) {
+                console.log("PUT do botão de editar clicado!")
+                console.log(response.status, response.statusText);
+                $scope.updateAllUsers();
+            }, function errorCallback(response) {
+                console.log("PUT do botão de editar deu ruim :(((");
+                console.log(response.status, response.statusText);
+            });
+           
+    }
+
+    $scope.editUser = function(user) {
+        $scope.userBeingEdited = angular.copy(user);
+        $scope.userURL = `${apiURL}${user.id}/`;
     }
 
     $scope.saveEditedUser = function() {
         $http.put($scope.userURL, $scope.userBeingEdited)
-                                                    .then(function successCallback(response) {
-                                                        console.log(response.status, response.statusText);
-                                                        user = $scope.userBeingEdited;
-                                                    }, function errorCallback(response) {
-                                                        console.log("Deu ruim :(((");
-                                                        console.log(response.status, response.statusText);
-                                                    });
+                                .then(function successCallback(response) {
+                                    console.log(response.status, response.statusText);
+                                    $scope.updateAllUsers();
+                                }, function errorCallback(response) {
+                                    console.log("Deu ruim :(((");
+                                    console.log(response.status, response.statusText);
+                                });
     }
 
-    // $scope.addNewUser = function() {
-    //     $scope    
-    // }
+    $scope.saveNewUser = function() {
+        $http.post(apiURL, $scope.newUser)
+            .then(function successCallback(response) {
+                console.log(response.status, response.statusText);
+                $scope.newUser = new Object();
+                $scope.updateAllUsers();
+            }, function errorCallback(response) {
+                console.log("Deu ruim :(((");
+                console.log(response.status, response.statusText);
+            });
+    }
+
+    $scope.modalDeleteUser = function(user) {
+        $scope.userToDelete = user;
+        $scope.userToDeleteURL = `${apiURL}${user.id}/`;
+    }
+
+    $scope.deleteUser = function() {
+        $http.delete($scope.userToDeleteURL)
+                                    .then(function successCallback(response) {
+                                        console.log(response.status, response.statusText);
+                                        $scope.updateAllUsers();
+                                    }), function errorCallback(response) {
+                                        console.log(response.status, response.statusText);
+                                    }
+    }
 
     $scope.orderByMe = function(customOrderBy) {
         $scope.customOrderBy = customOrderBy;
